6e0b683bc0521ce4b018b8f552824506d99d9a9e
Shawal khan <shawalkt7042@gmail.com>
2025-06-22 23:07:10 +0500

    Update server.py

--- server.py
+++ server.py
@@ -24,6 +24,6 @@ def generate():
     return jsonify(project_data)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
