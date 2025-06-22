ab4c2d863328a2e6e6afc56e3fecf771f77a4333
Shawal khan <shawalkt7042@gmail.com>
2025-06-22 16:45:41 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from app.generator import generate_project
 
@@ -24,4 +25,5 @@ def generate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
